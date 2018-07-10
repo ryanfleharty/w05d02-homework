3348dc19692a8b718be368358395a2ef43571bfc
Benjamin Kaplan <benjaminkaplanart@gmail.com>
2018-07-10 17:32:19 -0600

    connected my data and can see JSON in the browser and he looks great for his age

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 
 
 const app = express();
+const Pokemon = require("./models/pokemon");
 
 
 const port = 3000;
@@ -10,8 +11,10 @@ const port = 3000;
 
 app.get("/", (req, res) => {
 	res.send("welcome to the Pokemon app!");
-})
-
+});
+app.get("/pokemon", (req, res) => {
+	res.send(Pokemon);
+});
 
 
 
