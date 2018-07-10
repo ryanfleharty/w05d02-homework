4c3c90a989aa5fdca0fb9dcb9287f93e99f01b4a
Alexandre Dohrmann <alexandredohrmann@Alexandres-MacBook-Pro.local>
2018-07-10 16:24:04 -0600

    Commit 3 - My server is set up and running

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -1,1 +1,30 @@
-console.log();
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+
+app.get('/', (req, res) => {
+	res.send("Welcome to the Pokemon App");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+	console.log('Gotta catch \'em all at Port:', PORT);
+});
