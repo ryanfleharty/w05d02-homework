e55e0d4607fec2b2ca2b5bb62b83a8820ce7870d
Benjamin Kaplan <benjaminkaplanart@gmail.com>
2018-07-10 15:54:15 -0600

    my server is up and running

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+
+
+const app = express();
+
+
+const port = 3000;
+
+
+app.get("/", (req, res) => {
+	res.send("welcome to the Pokemon app!");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+	console.log("howdy cowboy")
+})
