076569134e13582a1980ade398265b879c6c1cc0
Kathleen Turner <kathleenturner@Kathleens-MacBook-Pro.local>
2018-07-10 17:04:43 -0600

    index list display

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -6,7 +6,8 @@ app.get('', (req, res) => {
 	res.send("Welcome to the Pokemon App!")
 });
 app.get("/pokemon", (req, res) => {
-	res.render("index.ejs")
+	res.render("index.ejs", 
+		{"pokemonList":pokemon});
 });
 
 
