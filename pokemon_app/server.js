b8fd48e8286892fd471f7a8dad9cb32109653e01
mac <mac@macs-MacBook-Pro.local>
2018-07-10 16:21:21 -0600

    All my files are created

---
+++ pokemon_app/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const app = express();
+
+const bodyParser = require("body-parser")
+
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+	console.log("Listening on port 3000!");
+
+});
+
+
