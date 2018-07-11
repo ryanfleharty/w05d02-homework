b777deb5a87ee5ec7c945989f09b6fb2da3556c2
Alexandre Dohrmann <alexandredohrmann@Alexandres-MacBook-Pro.local>
2018-07-10 18:16:29 -0600

    Commit 9 - created show views of each pokemon

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -10,7 +10,7 @@ app.get('/pokemon', (req, res) => {
 });
 
 app.get('/pokemon/:id', (req, res) => {
-	res.send(req.params.id);
+	res.render('show.ejs', {pokemonList: Pokemon[req.params.id]});
 });
 
 
