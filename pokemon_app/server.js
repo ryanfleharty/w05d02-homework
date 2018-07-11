3af3d280abb8f2583f83582d11b78b26b8734510
Michael Richards <michaelrichards@Michaels-MacBook-Pro.local>
2018-07-10 21:35:16 -0600

    Commit 6 - I can view a list of all my pokemon in the browser

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -10,9 +10,36 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Pokemon App!");
 });
 
-app.get("/pokemon", (req, res) => {
-  res.render("show.ejs");
+// Show Route
+app.get('/pokemon', (req, res) => {
+  // render is when you want to send
+  // an ejs template to the client
+  console.log(req.params)
+  res.render('show.ejs', {
+    pokemon: pokemon // This creates
+    // a "fruit" variable in the show page.
+  });
 });
+
+
+//app.get("/pokemon", (req, res) => {
+
+  // res.render("show.ejs", {
+  //   thesePokemon: pokemon
+  // });
+
+  // res.render("show.ejs", {
+  //   thesePokemon: pokemon
+  //
+  //   //console.log(req);
+  // });
+
+//   res.render('show.ejs', {
+//   fruit: Fruits[req.params.index] // This creates
+//   // a "fruit" variable in the show page.
+// });
+
+//});
 
 app.listen(PORT, () => {
   console.log("Welcome to the Pokemon App!");
