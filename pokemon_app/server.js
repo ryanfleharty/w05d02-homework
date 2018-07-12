6b636c0d30fc2ef376969cb1af7fe43380fd12e3
mac <mac@macs-MacBook-Pro.local>
2018-07-11 21:19:41 -0600

    finally added the last of the RESTful crap

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -16,25 +16,32 @@ app.use(methodOverride("_method"))
 
 
 
-app.get("/pokemon", (req, res) => {
+app.get('/pokemon', (req, res) => {
 	res.render("index.ejs", {"pokeList":Pokemon})
 });
 
-app.get("/pokemon/:id", (req, res) => {
+app.post('/pokemon', (req, res) => {
+  Pokemon.push(req.body);
+  res.redirect('/pokemon');
+});
+
+app.get('/pokemon/new', (req, res) => {
+  res.render('new.ejs');
+});
+
+app.get('/pokemon/:id', (req, res) => {
 	res.render("show.ejs" ,{pokemon:Pokemon[req.params.id]})
 })
 
-app.get("/pokemon/:id/edit", (req, res) =>{
+app.get('/pokemon/:id/edit', (req, res) =>{
   res.render("edit.ejs", {
   	pokemon: Pokemon[req.params.id], 
   	id:req.params.id})
-
 });
 
-app.put("/pokemon/:id", (req, res) => {
+app.put('/pokemon/:id', (req, res) => {
   Pokemon[req.params.id] = req.body;
   res.redirect("/pokemon");
-
 })
 
 app.delete('/pokemon/:id', (req, res) => {
