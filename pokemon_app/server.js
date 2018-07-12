d250a069b6beafca5413d0e0a7b9f027aad7c620
Amanda Molde <amanda.molde@gmail.com>
2018-07-11 19:59:16 -0600

    Created destroy and new routes

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -1,24 +1,53 @@
 const express = require('express');
 const app = express();
+
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+
 const Pokemon = require("./models/pokemon");
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res)=> {
 	res.send("Welcome to the Pokemon App!");
-})
+});
 
+// INDEX
 app.get('/pokemon', (req, res) => {
 	res.render('index.ejs', {
 		pokemon: Pokemon
 	});
-})
+});
 
+// CREATE
+app.post('/pokemon', (req, res) => {
+	Pokemon.push(req.body);
+	res.redirect('/pokemon');
+});
+
+app.get('/pokemon/new', (req, res) => {
+	res.render('new.ejs', {
+	});
+});
+
+// EDIT
+
+// SHOW
 app.get('/pokemon/:id', (req, res) => {
 	res.render('show.ejs', {
 		pokemon: Pokemon[req.params.id]
-	})
-})
+	});
+});
+
+// DELETE
+app.delete('/pokemon/:id', (req, res) => {
+	Pokemon.splice(req.params.id, 1);
+	res.redirect('/pokemon');
+});
+
 
 app.listen(3000, () => {
 	console.log("listening on port 3000");
