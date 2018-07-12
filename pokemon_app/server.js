152b8f7eb0e92883253cfc33f56cc65103ac1877
Kathleen Turner <kathleenturner@Kathleens-MacBook-Pro.local>
2018-07-12 00:02:14 -0600

    put not working

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const methodOverride = require('method-override');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+
+
 const pokemon = require("./models/pokemon")
 
 app.get('', (req, res) => {
@@ -14,6 +22,33 @@ app.get("/pokemon/:index", (req, res) => {
 		"pokemonList":pokemon[req.params.index]
 		})
 });
+
+app.post("/pokemon", (req, res) => {
+	pokemon.push(req.body);
+	res.redirect("/pokemon")
+});
+
+app.get("/new", (req, res) => {
+	res.render("new.ejs")
+});
+
+app.get("/pokemon/:index/edit", (req, res) => {
+	res.render("edit.ejs", {
+		"pokemonList": pokemon[req.params.index],
+		index: req.params.index 
+	})
+})
+app.put("/pokemon/:index/edit", (req, res) => {
+	pokemon[req.params.index] = req.body;
+	res.redirect("/pokemon");
+});
+
+app.delete("/pokemon/:index", (req, res) => {
+	pokemon.splice[req.params.index, 1]
+	res.redirect("/pokemon");
+	})
+
+
 
 
 
