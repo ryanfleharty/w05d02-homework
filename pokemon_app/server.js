86e79d6ed7fcd7bf326a8bcfa66632ba062c93b1
Michael Richards <michaelrichards@Michaels-MacBook-Pro.local>
2018-07-17 08:59:09 -0600

    Commit 8 - added dynamic anchor tags to index.ejs

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -10,25 +10,28 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Pokemon App!");
 });
 
-// Show Route
-app.get('/pokemon/show', (req, res) => {
-  // render is when you want to send
-  // an ejs template to the client
-
-  res.render('show.ejs', {
-    pokemon: pokemon
-  });
-});
-
+// shows all the pokemon
 app.get("/pokemon/index", (req, res) => {
   res.render('index.ejs', {
     pokemon: pokemon
   });
 });
 
+// Show Route
 app.get('/pokemon/:id', (req, res) => {
-  res.send(req.params.id);
+  // render is when you want to send
+  // an ejs template to the client
+
+  res.render('show.ejs', {
+    pokemon: pokemon[req.params.id],
+    id: req.params.id
+  });
 });
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log("Welcome to the Pokemon App!");
