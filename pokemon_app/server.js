85993b28b1159bf7d4ea749e72549037e5131264
Michael Richards <michaelrichards@Michaels-MacBook-Pro.local>
2018-07-17 12:42:20 -0600

    Commit 10 - set up serving of static files so we can add CSS

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+app.use(express.static('public'));
 
 const pokemon = require('./models/pokemon');
+
 
 
 app.get("/", (req, res) => {
